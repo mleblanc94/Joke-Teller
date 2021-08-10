a2ced46eb28fefb1ded5c59aca48a40ee4622934
mleblanc94 <leblancmichael94@gmail.com>
2021-08-09 20:47:50 -0400

    attempted changes

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let button = document.getElementById('button');
 
+
+async function getJoke() {
+    let apiUrl = 'https://v2.jokeapi.dev/joke/';
+       try {
+     const response = await fetch(apiUrl);
+     apiJoke = await response.json();
+     console.log(apiJoke);
+  } catch(error) {
+      console.log('Sorry, there was an error.');
+  }
+}
+
+  getJoke();
+
 let buttonClick = () => {
     console.log('it works');
 }
